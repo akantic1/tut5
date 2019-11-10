6b2e573fffcc26a838caeebe6ebf957ebf2b4ab0
akantic1 <akantic1@etf.unsa.ba>
2019-11-10 20:17:55 +0100

    zavrsena klasa Racun

--- src/com/rpr/t5/Racun.java
+++ src/com/rpr/t5/Racun.java
@@ -12,4 +12,48 @@ public class Racun {
         this.stanjeRacuna = 0d;
         this.odobrenjePrekoracenja = false;
     }
+
+    public Long getBrojRacuna() {
+        return brojRacuna;
+    }
+
+    public Osoba getKorisnikRacuna() {
+        return korisnikRacuna;
+    }
+
+    public Double getStanjeRacuna() {
+        return stanjeRacuna;
+    }
+
+    public boolean isOdobrenjePrekoracenja() {
+        return odobrenjePrekoracenja;
+    }
+
+    public boolean izvrsiUplatu(Double uplata){
+        stanjeRacuna += uplata;
+        return true;
+    }
+
+    public boolean izvrsiIsplatu(Double isplata){
+        if(stanjeRacuna - isplata < 0)
+            provjeriOdobrenjePrekoracenja(isplata);
+        else
+            stanjeRacuna -= isplata;
+        return true;
+
+    }
+
+    private boolean provjeriOdobrenjePrekoracenja(Double prekoracenje){
+        if (odobrenjePrekoracenja){
+            stanjeRacuna -= prekoracenje;
+            return true;
+        }
+        else return false;
+    }
+
+    public void odobriPrekoracenje(){
+        odobrenjePrekoracenja = true;
+    }
+
+
 }
